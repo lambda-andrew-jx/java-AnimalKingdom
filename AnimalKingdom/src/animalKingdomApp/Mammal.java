1fab9553d16a578438326b36716b8e7863fa1e6e
Sydney Crumley <srsimps19@gmail.com>
2019-06-05 16:40:19 -0500

    built out mammal class

--- AnimalKingdom/src/animalKingdomApp/Mammal.java
+++ AnimalKingdom/src/animalKingdomApp/Mammal.java
@@ -1,1 +1,14 @@
 package animalKingdomApp;
+
+public class Mammal extends AnimalAbstract
+{
+
+    public Mammal(String name, int discovered)
+    {
+        super(name, discovered);
+
+        setBreath("lungs");
+        setMovement("walking");
+        setReproduction("live births");
+    }
+}
